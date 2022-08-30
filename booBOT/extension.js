b9ae971865157754b24c9ea85546909c4419a7b4
kathrynzaro <zarokathryn@gmail.com>
2022-08-29 17:49:57 -0700

    adds startTimerFunction

--- booBOT/extension.js
+++ booBOT/extension.js
@@ -1,52 +1,64 @@
-const vscode = require("vscode");
-const getThemes = require("./fetch-utils.js");
+const vscode = require('vscode');
+const getThemes = require('./fetch-utils.js');
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 async function activate(context) {
-  getThemes().then(console.log);
+	getThemes().then(console.log);
 
-  const themes = await getThemes();
-  console.log("themes", themes);
+	const themes = await getThemes();
+	console.log('themes', themes);
 
-  function getHalloweenCountdown() {
-    const dayOfHalloween = new Date("October 31, 2022 12:00:00").getTime();
-    const today = new Date().getTime();
-    const secondsTillHalloween = dayOfHalloween - today;
-    const daysTillHalloween = Math.floor(
-      secondsTillHalloween / 1000 / 60 / 60 / 24
-    );
-    return daysTillHalloween;
-  }
-  const daysTillHalloween = getHalloweenCountdown();
+	function getHalloweenCountdown() {
+		const dayOfHalloween = new Date('October 31, 2022 12:00:00').getTime();
+		const today = new Date().getTime();
+		const secondsTillHalloween = dayOfHalloween - today;
+		const daysTillHalloween = Math.floor(
+			secondsTillHalloween / 1000 / 60 / 60 / 24
+		);
+		return daysTillHalloween;
+	}
+	const daysTillHalloween = getHalloweenCountdown();
 
-  let disposable = vscode.commands.registerCommand(
-    "booBOT.helloWorld",
+	async function startTimerFunction() {
+		setInterval(async () => {
+			// const themes = await getThemes();
+			vscode.window.showInformationMessage(
+				`BOO! Did I scare you? Here's a spooky fact: template literal goes here. Would you like an extra treat? `,
+				'Take me to the treat!',
+				'No, life is spooky enough.'
+			);
+		}, 60000);
+	}
 
-    async function responseFunction() {
-      console.log("daysTillHalloween", daysTillHalloween);
-      const response = await vscode.window.showInformationMessage(
-        `Welcome to boo!BOT. There are ${daysTillHalloween} days until Halloween. Would you like haunted enCounters today? `,
-        "Spook me!",
-        "No, life is spooky enough."
-      );
-      if (response === "Spook me!") {
-        vscode.window.showInformationMessage("Ok, we'll haunt you shortly!");
-      } else if (response === "No, life is spooky enough.") {
-        vscode.window.showInformationMessage(
-          "Ok, hope you don't have any boos in your code today!"
-        );
-      }
-    }
-  );
+	let disposable = vscode.commands.registerCommand(
+		'booBOT.helloWorld',
 
-  context.subscriptions.push(disposable);
+		async function responseFunction() {
+			console.log('daysTillHalloween', daysTillHalloween);
+			const response = await vscode.window.showInformationMessage(
+				`Welcome to boo!BOT. There are ${daysTillHalloween} days until Halloween. Would you like haunted enCounters today? `,
+				'Spook me!',
+				'No, life is spooky enough.'
+			);
+			if (response === 'Spook me!') {
+				vscode.window.showInformationMessage("Ok, we'll haunt you shortly!");
+				startTimerFunction();
+			} else if (response === 'No, life is spooky enough.') {
+				vscode.window.showInformationMessage(
+					"Ok, hope you don't have any boos in your code today!"
+				);
+			}
+		}
+	);
+
+	context.subscriptions.push(disposable);
 }
 
 function deactivate() {}
 
 module.exports = {
-  activate,
-  deactivate,
+	activate,
+	deactivate,
 };
