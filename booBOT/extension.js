2bc0eba2b3c5ea666aaf8483170c0fee3eccb57d
kathrynzaro <zarokathryn@gmail.com>
2022-08-31 12:03:19 -0700

    adds countdown to status bar

--- booBOT/extension.js
+++ booBOT/extension.js
@@ -1,82 +1,91 @@
-const vscode = require("vscode");
-const { getRandomFact, getRandomLink } = require("./fetch-utils.js");
+const vscode = require('vscode');
+const { getRandomFact, getRandomLink } = require('./fetch-utils.js');
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 
-
-
 async function activate(context) {
-  function getHalloweenCountdown() {
-    let currentYear = new Date().getFullYear();
-    let dayOfHalloween = new Date(`October 31, ${currentYear} 12:00:00`).getTime();
-    const today = new Date().getTime();
-    let secondsTillHalloween = dayOfHalloween - today;
-    if(secondsTillHalloween <0){
-      currentYear ++
-      dayOfHalloween = new Date(`October 31, ${currentYear} 12:00:00`).getTime();
-      secondsTillHalloween = dayOfHalloween - today;
-    }
-    const daysTillHalloween = Math.floor(
-      secondsTillHalloween / 1000 / 60 / 60 / 24
-      
-    );
-    console.log('daysTillHalloween', daysTillHalloween)
-    return daysTillHalloween;
-  }
-  const daysTillHalloween = getHalloweenCountdown();
+	const booStatusItem = vscode.window.createStatusBarItem(
+		vscode.StatusBarAlignment.Right
+	);
+	// context.subscriptions.push(booStatusItem);
 
-  const oneMinute = 1000 * 60;
-  const timeInterval = Number(
-    vscode.workspace.getConfiguration("booBOT").get("timeInterval")
-  );
-  const booInterval = oneMinute * timeInterval;
+	function getHalloweenCountdown() {
+		let currentYear = new Date().getFullYear();
+		let dayOfHalloween = new Date(
+			`October 31, ${currentYear} 12:00:00`
+		).getTime();
+		const today = new Date().getTime();
+		let secondsTillHalloween = dayOfHalloween - today;
+		if (secondsTillHalloween < 0) {
+			currentYear++;
+			dayOfHalloween = new Date(
+				`October 31, ${currentYear} 12:00:00`
+			).getTime();
+			secondsTillHalloween = dayOfHalloween - today;
+		}
+		const daysTillHalloween = Math.floor(
+			secondsTillHalloween / 1000 / 60 / 60 / 24
+		);
+		console.log('daysTillHalloween', daysTillHalloween);
+		return daysTillHalloween;
+	}
+	const daysTillHalloween = getHalloweenCountdown();
 
-  async function startTimerFunction() {
-    setInterval(async () => {
-      const facts = await getRandomFact();
-      const links = await getRandomLink();
-      const response = await vscode.window.showInformationMessage(
-        `BOO! Did I scare you? Here's a spooky treat: ${facts.content} Would you like an extra treat? `,
-        "Take me to the treat!",
-        "No, life is spooky enough."
-      );
-      if (response === "Take me to the treat!") {
-        vscode.env.openExternal(vscode.Uri.parse(`${links.url}`));
-      } else if (response === "No, life is spooky enough.") {
-        vscode.window.showInformationMessage("Ok, see you next haunting...");
-      }
-    }, booInterval);
-  }
+	booStatusItem.text = `👻 ${daysTillHalloween} days til Halloween!`;
+	booStatusItem.show();
 
-  const response = await vscode.window.showInformationMessage(
-    `Welcome to boo!BOT. There are ${daysTillHalloween} days until Halloween. Would you like haunted enCounters today? `,
-    "Spook me!",
-    "No, life is spooky enough."
-  );
-  if (response === "Spook me!") {
-    vscode.window.showInformationMessage("Ok, we'll haunt you shortly!");
-    startTimerFunction();
-  } else if (response === "No, life is spooky enough.") {
-    vscode.window.showInformationMessage(
-      "Ok, hope you don't have any boos in your code today!"
-    );
-  }
+	const oneMinute = 1000 * 60;
+	const timeInterval = Number(
+		vscode.workspace.getConfiguration('booBOT').get('timeInterval')
+	);
+	const booInterval = oneMinute * timeInterval;
 
-  let disposable = vscode.commands.registerCommand(
-    "booBOT.helloWorld",
-    function () {
-      activate(context);
-    }
-  );
+	async function startTimerFunction() {
+		setInterval(async () => {
+			const facts = await getRandomFact();
+			const links = await getRandomLink();
+			const response = await vscode.window.showInformationMessage(
+				`BOO! ༼ つ ╹ ╹ ༽つ Did I scare you? Here's a spooky treat: ${facts.content} Would you like an extra treat? `,
+				'Take me to the treat!',
+				'No, life is spooky enough.'
+			);
+			if (response === 'Take me to the treat!') {
+				vscode.env.openExternal(vscode.Uri.parse(`${links.url}`));
+			} else if (response === 'No, life is spooky enough.') {
+				vscode.window.showInformationMessage('Ok, see you next haunting...');
+			}
+		}, booInterval);
+	}
 
-  context.subscriptions.push(disposable);
+	const response = await vscode.window.showInformationMessage(
+		`Welcome to boo!BOT. There are ${daysTillHalloween} days until Halloween. Would you like haunted enCounters today? `,
+		'Spook me!',
+		'No, life is spooky enough.'
+	);
+	if (response === 'Spook me!') {
+		vscode.window.showInformationMessage("Ok, we'll haunt you shortly!");
+		startTimerFunction();
+	} else if (response === 'No, life is spooky enough.') {
+		vscode.window.showInformationMessage(
+			"Ok, hope you don't have any boos in your code today!"
+		);
+	}
+
+	let disposable = vscode.commands.registerCommand(
+		'booBOT.helloWorld',
+		function () {
+			activate(context);
+		}
+	);
+
+	context.subscriptions.push(disposable);
 }
 
 function deactivate() {}
 
 module.exports = {
-  activate,
-  deactivate,
+	activate,
+	deactivate,
 };
